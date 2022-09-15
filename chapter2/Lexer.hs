f9ebd064d2d803cf7fade0d1320e2e6ca4659c71
Andrés Collares <andres.collares@fing.edu.uy>
2022-09-15 20:15:04 -0300

    Initial commit

---
+++ chapter2/Lexer.hs
@@ -0,0 +1,41 @@
+module Lexer where
+
+import Text.Parsec.Language (emptyDef)
+import Text.Parsec.String (Parser)
+import qualified Text.Parsec.Token as Tok
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where
+    ops = ["+", "*", "-", ";"]
+    names = ["def", "extern"]
+    style =
+      emptyDef
+        { Tok.commentLine = "#",
+          Tok.reservedOpNames = ops,
+          Tok.reservedNames = names
+        }
+
+integer :: Parser Integer
+integer = Tok.integer lexer
+
+float :: Parser Double
+float = Tok.float lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+semiSep :: Parser a -> Parser [a]
+semiSep = Tok.semiSep lexer
+
+commaSep :: Parser a -> Parser [a]
+commaSep = Tok.commaSep lexer
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
