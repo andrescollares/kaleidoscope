521ffe3998c01ab3212e570388db3375e2c18ac5
Agustin Gazzano <agustingazzano107@gmail.com>
2023-06-04 15:42:39 -0300

    some more unit tests

--- kaleidoscope/test/Spec.hs
+++ kaleidoscope/test/Spec.hs
@@ -22,31 +22,29 @@ assertAST code ast = do
         expressions <- mapM_ print ex
         show ex @?= ast
 
+testProgram :: String -> IO ()
+testProgram programName = do
+  source <- readFile $ "./test/programs/" ++ programName ++ ".k"
+  expectedAST <- readFile $ "./test/parsed/" ++ programName
+  assertAST source expectedAST
+
 parserTests :: TestTree
 parserTests = testGroup "Parser Tests"
   [
-    testCase "add" $ assertAST 
-      "def add(x y) x + y; add(9, 10);"
-      "[Function \"add\" [\"x\",\"y\"] (BinOp \"+\" (Var \"x\") (Var \"y\")),Call \"add\" [Float 9.0,Float 10.0]]"
-    , testCase "sub" $ assertAST 
-      "def sub(x y) x - y; sub(9, 10);"
-      "[Function \"sub\" [\"x\",\"y\"] (BinOp \"-\" (Var \"x\") (Var \"y\")),Call \"sub\" [Float 9.0,Float 10.0]]"
-    , testCase "add_sub" $ assertAST 
-      "def add(x y) x + y;\
-       \ def sub(x y) x - y;\
-       \ add(10, sub(6, 1));"
-      "[Function \"add\" [\"x\",\"y\"] (BinOp \"+\" (Var \"x\") (Var \"y\")),Function \"sub\" [\"x\",\"y\"] (BinOp \"-\" (Var \"x\") (Var \"y\")),Call \"add\" [Float 10.0,Call \"sub\" [Float 6.0,Float 1.0]]]"
-    , testCase "var_in" $ assertAST
-      "def binary : 1 (x y) y;\
-
-      \def circlearea(r) \
-          \var pi = 3.14 in \
-          \pi * r * r;\
-
-      \var dos = 10000 in \
-          \dos + circlearea(2) :\
-          \circlearea(dos);"
-      "[BinaryDef \":\" [\"x\",\"y\"] (Var \"y\"),Function \"circlearea\" [\"r\"] (Let \"pi\" (Float 3.14) (BinOp \"*\" (BinOp \"*\" (Var \"pi\") (Var \"r\")) (Var \"r\"))),Let \"dos\" (Float 10000.0) (BinOp \":\" (BinOp \"+\" (Var \"dos\") (Call \"circlearea\" [Float 2.0])) (Call \"circlearea\" [Var \"dos\"]))]"
+    testCase "add" $ do testProgram "add"
+    , testCase "add_sub" $ do testProgram "add_sub"
+    , testCase "factorial_print" $ do testProgram "factorial_print"
+    , testCase "factorial" $ do testProgram "factorial"
+    , testCase "fib_iterative" $ do testProgram "fib_iterative"
+    , testCase "fib" $ do testProgram "fib"
+    , testCase "hello_world" $ do testProgram "hello_world"
+    , testCase "id" $ do testProgram "id"
+    , testCase "pow_operator" $ do testProgram "pow_operator"
+    , testCase "pow" $ do testProgram "pow"
+    , testCase "sequence_operator" $ do testProgram "sequence_operator"
+    , testCase "sub" $ do testProgram "sub"
+    , testCase "unary_minus" $ do testProgram "unary_minus"
+    , testCase "var_in" $ do testProgram "var_in"
   ]
 
 -- TODO: add RunJIT examples to compare llvm output
