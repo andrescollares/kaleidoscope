2aec0ee19d72cbbede15f90580e199a4035698d8
Andrés Collares <andres.collares@fing.edu.uy>
2023-10-01 19:29:07 -0300

    IRBuilder and formatting changes

---
+++ kaleidoscope/src/IRBuilder.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo #-}
+
+module IRBuilder where
+
+import Data.ByteString.Short
+import LLVM.AST hiding (function)
+import qualified LLVM.AST.Constant as C
+import LLVM.AST.Float
+import qualified LLVM.AST.Float as F
+import qualified LLVM.AST.IntegerPredicate as P
+import LLVM.AST.Type as AST
+import LLVM.IRBuilder.Instruction
+import LLVM.IRBuilder.Module
+import LLVM.IRBuilder.Monad
+
+simple :: Module
+simple = buildModule "exampleModule" $ do
+  function "plus" [(AST.i32, "x"), (AST.i32, "y")] AST.i32 $ \[x, y] -> do
+    _entry <- block `named` "entry2"
+    r <- add x y
+    ret r
+
+condition :: Module
+condition = buildModule "conditionModule" $ do
+  function "f" [(AST.i32, "a")] AST.i32 $ \[a] -> mdo
+    _entry <- block `named` "entry"
+    cond <- icmp P.EQ a (ConstantOperand (C.Int 32 0))
+    condBr cond ifThen ifElse
+    ifThen <- block
+    trVal <- add a (ConstantOperand (C.Int 32 0))
+    br ifExit
+    ifElse <- block `named` "if.else"
+    flVal <- add a (ConstantOperand (C.Int 32 0))
+    br ifExit
+    ifExit <- block `named` "if.exit"
+    r <- phi [(trVal, ifThen), (flVal, ifElse)]
+    ret r
+
+arithmetrics :: Module
+arithmetrics = buildModule "arithmetrics" $ do
+  function "+" [(AST.i32, "x"), (AST.i32, "y")] AST.i32 $ \[x, y] -> do
+    entry <- block `named` "entry"
+    r <- add x y
+    ret r
+
+  function "-" [(AST.i32, "x"), (AST.i32, "y")] AST.i32 $ \[x, y] -> do
+    entry <- block `named` "entry2"
+    r <- sub x y
+    ret r
+
+  function "*" [(AST.i32, "x"), (AST.i32, "y")] AST.i32 $ \[x, y] -> do
+    entry <- block `named` "entry3"
+    r <- mul x y
+    ret r
+
+  function "/" [(AST.i32, "x"), (AST.i32, "y")] AST.i32 $ \[x, y] -> do
+    entry <- block `named` "entry4"
+    r <- sdiv x y
+    ret r
+
+globalDef :: Module
+globalDef = buildModule "variable_test" $ do
+  x <- global "x" AST.double (C.Float (Double 50.0))
+
+  -- function "f" [] AST.i32 $ \[] -> do
+  --   entry <- block `named` "entry"
+  --   r <- add x1 (ConstantOperand (C.Int 32 1))
+  --   ret r
+
+  function "main" [] AST.double $ \[] -> do
+    entry <- block `named` "entry"
+    -- x1 <- load x 64
+    r <- fadd (ConstantOperand (C.Float (Double 1.0))) (ConstantOperand (C.Float (Double 1.0)))
+    -- res <- call (ConstantOperand (C.GlobalReference (ptr (FunctionType AST.i32 [] False)) (Name "f"))) []
+    ret r
--- kaleidoscope/src/JIT.hs
+++ kaleidoscope/src/JIT.hs
@@ -2,25 +2,21 @@
 
 module JIT where
 
+import Control.Monad.Except
+import qualified Data.ByteString as BS
 import Data.Int
 import Data.Word
-import Foreign.Ptr ( FunPtr, castFunPtr )
-
-import Control.Monad.Except
-
-import LLVM.Target
-import LLVM.Context
-import LLVM.CodeModel
-import LLVM.Module as Mod
-import qualified LLVM.AST as AST
-
-import LLVM.PassManager
-import LLVM.Transforms
-import LLVM.Analysis
-
-import qualified LLVM.ExecutionEngine as EE
-import qualified Data.ByteString as BS
+import Foreign.Ptr (FunPtr, castFunPtr)
 import GHC.Base (Double)
+import qualified LLVM.AST as AST
+import LLVM.Analysis
+import LLVM.CodeModel
+import LLVM.Context
+import qualified LLVM.ExecutionEngine as EE
+import LLVM.Module as Mod
+import LLVM.PassManager
+import LLVM.Target
+import LLVM.Transforms
 
 foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> (IO Double)
 
@@ -30,13 +26,13 @@ run fn = haskFun (castFunPtr fn :: FunPtr (IO Double))
 jit :: Context -> (EE.MCJIT -> IO a) -> IO a
 jit c = EE.withMCJIT c optlevel model ptrelim fastins
   where
-    optlevel = Just 0  -- optimization level
-    model    = Nothing -- code model ( Default )
-    ptrelim  = Nothing -- frame pointer elimination
-    fastins  = Nothing -- fast instruction selection
+    optlevel = Just 0 -- optimization level
+    model = Nothing -- code model ( Default )
+    ptrelim = Nothing -- frame pointer elimination
+    fastins = Nothing -- fast instruction selection
 
 passes :: PassSetSpec
-passes = defaultCuratedPassSetSpec { optLevel = Just 3 }
+passes = defaultCuratedPassSetSpec {optLevel = Just 3}
 
 optimizeModule :: AST.Module -> IO (AST.Module)
 optimizeModule mod = do
@@ -55,14 +51,14 @@ optimizeModule mod = do
 
 runJIT :: AST.Module -> IO Double
 runJIT mod = do
-    withContext $ \context ->
-      jit context $ \executionEngine ->
-        withModuleFromAST context mod $ \m ->
-          EE.withModuleInEngine executionEngine m $ \ee -> do
-            mainfn <- EE.getFunction ee (AST.Name "main")
-            case mainfn of
-              Just fn -> do
-                result <- run fn
-                putStrLn $ "Evaluated to: " ++ show result
-                return result
-              Nothing -> return 0
+  withContext $ \context ->
+    jit context $ \executionEngine ->
+      withModuleFromAST context mod $ \m ->
+        EE.withModuleInEngine executionEngine m $ \ee -> do
+          mainfn <- EE.getFunction ee (AST.Name "main")
+          case mainfn of
+            Just fn -> do
+              result <- run fn
+              putStrLn $ "Evaluated to: " ++ show result
+              return result
+            Nothing -> return 0
